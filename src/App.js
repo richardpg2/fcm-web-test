812f57a1f92891962e6bc1187f191e0d954cf836
Richard <richard.pg202@gmail.com>
2023-07-30 11:02:16 +0700

    copy token

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
-import logo from './assets/sparky-dash-high-five.gif';
-import { getFirebaseToken, onForegroundMessage } from './firebase';
+import logo from "./assets/sparky-dash-high-five.gif";
+import { getFirebaseToken, onForegroundMessage } from "./firebase";
 
+import { useCopyClipboard } from "./useCopy";
 export default function App() {
-  const [showNotificationBanner, setShowNotificationBanner] = useState(Notification.permission === 'default');
+  const [showNotificationBanner, setShowNotificationBanner] = useState(Notification.permission === "default");
 
+  const [token, setToken] = useState("");
+
+  const [isCopied, copy] = useCopyClipboard(200);
   useEffect(() => {
     onForegroundMessage()
       .then((payload) => {
-        console.log('Received foreground message: ', payload);
-        const { notification: { title, body } } = payload;
+        console.log("Received foreground message: ", payload);
+        const {
+          notification: { title, body },
+        } = payload;
         toast(<ToastifyNotification title={title} body={body} />);
       })
-      .catch(err => console.log('An error occured while retrieving foreground message. ', err));
+      .catch((err) => console.log("An error occured while retrieving foreground message. ", err));
   }, []);
 
   const handleGetFirebaseToken = () => {
     getFirebaseToken()
       .then((firebaseToken) => {
-        console.log('Firebase token: ', firebaseToken);
+        console.log("Firebase token: ", firebaseToken);
+        setToken(firebaseToken);
         if (firebaseToken) {
           setShowNotificationBanner(false);
         }
       })
-      .catch((err) => console.error('An error occured while retrieving firebase token. ', err))
-  }
+      .catch((err) => console.error("An error occured while retrieving firebase token. ", err));
+  };
 
   const ToastifyNotification = ({ title, body }) => (
     <div className="push-notification">
@@ -37,16 +44,23 @@ export default function App() {
 
   return (
     <div className="app">
-      {showNotificationBanner && <div className="notification-banner">
-        <span>The app needs permission to</span>
-        <a
-          href="#"
-          className="notification-banner-link"
-          onClick={handleGetFirebaseToken}
-        >
-          enable push notifications.
-        </a>
-      </div>}
+      {showNotificationBanner && (
+        <div className="notification-banner">
+          <span>The app needs permission to</span>
+          <a href="#" className="notification-banner-link" onClick={handleGetFirebaseToken}>
+            enable push notifications.
+          </a>
+        </div>
+      )}
+
+      <a href="#" className="notification-banner-link" onClick={handleGetFirebaseToken}>
+        Show token
+      </a>
+
+      <p>Token: {token}</p>
+      <button onClick={() => copy(token)} class="btn-primary">
+        copy token
+      </button>
 
       <img src={logo} className="app-logo" alt="logo" />
 
